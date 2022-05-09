50b87d42e5e2af7974f5f9a2bd5482213a61e11f
Omshree-Dalvi-NeoSOFT <omshree.dalvi@neosoftmail.com>
2022-05-09 14:46:34 +0530

    Slice update

--- 06slices/main.go
+++ 06slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Welcome to slices")
@@ -15,4 +18,25 @@ func main() {
 
 	fruitList = append(fruitList[:2])
 	fmt.Println(fruitList)
+
+	highScore := make([]int, 4)
+
+	highScore[0] = 89
+	highScore[1] = 97
+	highScore[2] = 49
+	highScore[3] = 79
+	//highScore[4] = 69
+
+	fmt.Println(highScore)
+
+	highScore = append(highScore, 69, 22, 74)
+
+	fmt.Println(highScore)
+
+	sort.Ints(highScore)
+	fmt.Println(highScore)
+
+	highScore = append(highScore[:3], highScore[4:]...)
+	fmt.Println(highScore)
+
 }
