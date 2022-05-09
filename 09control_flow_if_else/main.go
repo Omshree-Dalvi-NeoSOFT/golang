776e5a62f16f12c47a772d2c97d4a532ca870e42
Omshree-Dalvi-NeoSOFT <omshree.dalvi@neosoftmail.com>
2022-05-09 16:48:18 +0530

    if-else

---
+++ 09control_flow_if_else/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to IF-ELSE conditional stmnt..")
+
+	if num := 30; num < 20 {
+		fmt.Println("Number is less then 20 ")
+	} else {
+		fmt.Println("Number is greater then 20")
+	}
+}
